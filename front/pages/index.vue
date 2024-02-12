0f396e0db044fffe3b92931e9052f016e3871729
takuya kishi <56611951+takuya2816@users.noreply.github.com>
2024-02-12 22:45:59 +0900

    20240212 commit

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,58 +1,22 @@
 <!-- LINEIDから顧客・提供者振り分けて各トップ画面へリダイレクト -->
 
 <template>
-    <div>   
+    <div>
+        <button @click="supplier">supplier</button>
+        <button @click="customer">customer</button>
     </div>
 </template>
 
 <script>
+// LINEIDから顧客・提供者振り分けて各トップ画面へリダイレクト
 import axios from 'axios';
-
 export default {
-    data() {
-        return {
-            accessToken: "",
-        }
-    },
-    mounted() {
-        this.$liffInit
-        .then(() => {
-            pass
-        })
-        .catch((error) => {
-            this.liffError = error;
-        });
-    },
-
     methods: {
-        async getAccessToken(){  // ユーザのaccesstokenを取得
-            try {
-                const accessToken = liff.getAccessToken();
-                return accessToken
-            } catch (error) {
-                console.error(error);
-                return null
-            }
+        customer() {
+            this.$router.push('/customer');
         },
-        async getUserProfile(accessToken) {  // ユーザ情報を取得
-            try {
-                const response = await axios.get(`https://api.line.me/v2/profile`, {
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`
-                    }
-                });
-                this.lineId = response.data.userId;  // 各種を更新
-                this.displayName = response.data.displayName;
-                this.pictureUrl = response.data.pictureUrl;
-            } catch (error) {
-                console.error(error);
-            }
-        },
-        async getUserInfo() {
-            const accessToken = await this.getAccessToken();
-            if (accessToken) {
-                await this.getUserProfile(accessToken);
-            }
+        supplier() {
+            this.$router.push('/supplier');
         },
     },
 };
