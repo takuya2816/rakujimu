2c45d50fccd70df9fbc80df658c72c97bfe7251c
takuya kishi <56611951+takuya2816@users.noreply.github.com>
2024-02-12 22:48:45 +0900

    20240212 commit

--- front/plugins/common.js
+++ front/plugins/common.js
@@ -1,5 +1,28 @@
+import axios from 'axios';
 export default {
     outputLog(message) {
         console.log(`LOG: ${message}`)
-    }
+    },
+
+    async gateway_get(apiurl){
+        try {
+            const response = await axios.get(apiurl);
+            return response.data;
+        } catch (error) {
+            alert("Error: " + error);
+        }
+    },
+    async gateway_post(apiurl, data){
+        var jsonData = JSON.stringify(data);
+        var headers =  {
+            headers: {'Content-Type': 'application/json'}
+        };
+        try {
+            const response = await axios.post(apiurl, jsonData, headers);
+            console.log(response);
+            return response.data;
+        } catch (error) {
+            alert("Error: " + error);
+        }
+    },
 }
